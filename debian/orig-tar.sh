776ce936d76a3c7aad35c64f4c01b57f68113a6a
Hans-Christoph Steiner <hans@eds.org>
2014-05-12 20:57:40 -0400

    rename DIRECTORY var to gitclonedir for clarity

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 REVISION=21
-DIRECTORY=androidsystemcore
+gitclonedir=androidsystemcore
 
-if [ -d $DIRECTORY -o -f $DIRECTORY ]
+if [ -d $gitclonedir -o -f $gitclonedir ]
 then
-    echo "The file/directory '$DIRECTORY' exists .. aborting."
+    echo "The file/directory '$gitclonedir' exists .. aborting."
     exit
 fi
 
 
-git clone -b tools_r$REVISION https://android.googlesource.com/platform/system/core/ $DIRECTORY
+git clone -b tools_r$REVISION https://android.googlesource.com/platform/system/core/ $gitclonedir
 
-VERSION=`git --git-dir=$DIRECTORY/.git log -1 --format=%cd~%h --date=short | sed s/-//g`
+VERSION=`git --git-dir=$gitclonedir/.git log -1 --format=%cd~%h --date=short | sed s/-//g`
 
 echo "Version $VERSION"
 
 # now we delete the files that we dont intend to use
 echo "Deleting not needed files ..."
-rm -fr `find $DIRECTORY -maxdepth 1 -type d ! -name $DIRECTORY ! -name liblog ! -name libcutils ! -name include ! -name libzipfile`
-rm -fr `find $DIRECTORY/include -maxdepth 1 -type d ! -name include ! -name cutils ! -name android ! -name zipfile`
+rm -fr `find $gitclonedir -maxdepth 1 -type d ! -name $gitclonedir ! -name liblog ! -name libcutils ! -name include ! -name libzipfile`
+rm -fr `find $gitclonedir/include -maxdepth 1 -type d ! -name include ! -name cutils ! -name android ! -name zipfile`
 
 tarballdir=$(cd `dirname $0`/../../; pwd)
 
-echo "Packaging archive into ../${DIRECTORY}_$REVISION+git$VERSION.orig.tar.gz ..."
-tar -czf ${tarballdir}/${DIRECTORY}_$REVISION+git$VERSION.orig.tar.gz $DIRECTORY
-echo "Deleting folder '$DIRECTORY'"
-rm -rf $DIRECTORY
+echo "Packaging archive into ../${gitclonedir}_$REVISION+git$VERSION.orig.tar.gz ..."
+tar -czf ${tarballdir}/${gitclonedir}_$REVISION+git$VERSION.orig.tar.gz $gitclonedir
+echo "Deleting folder '$gitclonedir'"
+rm -rf $gitclonedir
 

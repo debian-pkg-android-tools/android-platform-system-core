c9973053ab955a8244676c428fcf4f67df1a08a8
Kai-Chung Yan <seamlikok@gmail.com>
2016-02-16 14:34:14 +0800

    Clean unused flags in makefiles

--- debian/liblog.mk
+++ debian/liblog.mk
@@ -2,9 +2,8 @@ NAME = liblog
 SOURCES = fake_log_device.c logd_write_kern.c logprint.c uio.c event_tag_map.c
 SOURCES := $(foreach source, $(SOURCES), liblog/$(source))
 CFLAGS += -fPIC
-CPPFLAGS += -include android/arch/AndroidConfig.h -Iinclude
-LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 \
-           -Wl,-rpath=/usr/lib/android
+CPPFLAGS += -include android/arch/AndroidConfig.h -Iinclude -DLIBLOG_LOG_TAG=1005
+LDFLAGS += -shared -Wl,-soname,$(NAME).so.0
 
 build: $(SOURCES)
 	$(CC) $^ -o $(NAME).so.0 $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
--- debian/libsparse.mk
+++ debian/libsparse.mk
@@ -3,8 +3,7 @@ SOURCES = backed_block.c output_file.c sparse.c sparse_crc32.c sparse_err.c spar
 SOURCES := $(foreach source, $(SOURCES), libsparse/$(source))
 CFLAGS += -fPIC
 CPPFLAGS += -include android/arch/AndroidConfig.h -Iinclude -Ilibsparse/include
-LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 \
-           -Wl,-rpath=/usr/lib/android -lz
+LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 -lz
 
 build: $(SOURCES)
 	$(CC) $^ -o $(NAME).so.0 $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)

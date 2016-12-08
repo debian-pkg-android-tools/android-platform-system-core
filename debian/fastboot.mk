1fb277a8af4125246737f92f91aad5169d8993b2
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-12-08 13:51:50 +0800

    Full stage build (with fastboot)

--- debian/fastboot.mk
+++ debian/fastboot.mk
@@ -1,32 +1,33 @@
 NAME = fastboot
-SOURCES = protocol.c engine.c bootimg_utils.cpp fastboot.cpp util.c fs.c usb_linux.c util_linux.c
-CSOURCES = $(foreach source, $(filter %.c, $(SOURCES)), fastboot/$(source))
-CXXSOURCES = $(foreach source, $(filter %.cpp, $(SOURCES)), fastboot/$(source))
-COBJECTS = $(CSOURCES:.c=.o)
-CXXOBJECTS = $(CXXSOURCES:.cpp=.o)
-CFLAGS += -c
-CXXFLAGS += -c -fpermissive
+SOURCES = bootimg_utils.cpp \
+          engine.cpp \
+          fastboot.cpp \
+          fs.cpp\
+          protocol.cpp \
+          socket.cpp \
+          tcp.cpp \
+          udp.cpp \
+          util.cpp \
+          usb_linux.cpp \
+          util_linux.cpp
+SOURCES := $(foreach source, $(SOURCES), fastboot/$(source))
+CXXFLAGS += -fpermissive
 CPPFLAGS += -DUSE_F2FS -DFASTBOOT_REVISION='"debian"' \
-            -include android/arch/AndroidConfig.h \
             -Iinclude \
             -Imkbootimg \
+            -Iadb \
+            -Ibase/include \
             -I/usr/include/android/ext4_utils \
             -I/usr/include/android/f2fs_utils \
             -I/usr/include/openssl \
             -Ilibsparse/include
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -Wl,-rpath-link=. \
-           -L. -lziparchive -lsparse \
+           -L. -lziparchive -lsparse -lbase -lcutils -ladb \
            -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -lext4_utils -lf2fs_utils
 
-build: $(COBJECTS) $(CXXOBJECTS)
-	$(CC) $^ -o fastboot/$(NAME) $(LDFLAGS)
+build: $(SOURCES)
+	$(CXX) $^ -o fastboot/$(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
 
 clean:
-	$(RM) fastboot/$(NAME) $(COBJECTS) $(CXXOBJECTS)
-
-$(CXXOBJECTS): %.o: %.cpp
-	$(CXX) $< -o $@ $(CXXFLAGS) $(CPPFLAGS)
-
-$(COBJECTS): %.o: %.c
-	$(CC) $< -o $@ $(CFLAGS) $(CPPFLAGS)
+	$(RM) fastboot/$(NAME)
